5928b99d069ac0971d04464194d29cfb015b3019
fernanda oliveira <fer.nand.2149@bol.com.br>
2021-09-22 23:55:53 -0300

    Configuração de CORS para qualquer metodo e cabeçalho

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,9 @@ namespace API
         services.AddCors(
             options =>{ 
                     options.AddPolicy("CorsPolicy", builder => builder
-                    .AllowAnyOrigin());
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
             }
         );
 
